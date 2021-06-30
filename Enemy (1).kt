82a01451f7707edcc8f105ac5dbeb9b4df610f6d
mitchel030 <72606177+mitchel030@users.noreply.github.com>
2021-06-30 13:24:48 +0200

    "Enemy updated"
    "Enemy updated"

---
+++ Enemy (1).kt
@@ -0,0 +1,29 @@
+
+open class Enemy(var name: String, var life: Int = 1, var hitpoints: Int = 5) {
+    open fun takeDamage(damage: Int) {
+        val remainingHitpoints = hitpoints - damage
+        if (remainingHitpoints > 0){
+            hitpoints = remainingHitpoints
+            println(" je hebt $damage damage tegen je gekregen. je hebt nog $hitpoints hitpoints")
+        }else {
+            if (life < 1){
+                life -= 1
+                println(" je bent een leven kwijt")
+            }else {
+                life -= 1
+                println("je tegenstander is dood")
+            }
+        }
+    }
+    override fun toString(): String {
+        return  """
+           name:        $name
+           hitpoints:   $hitpoints
+           life:        $life
+                """
+    }
+    open fun attack(character: Character, minDamage: Int, maxDamage: Int){
+        val attackDamage = (minDamage..maxDamage).random()
+        character.takeDamage(attackDamage)
+    }
+}
